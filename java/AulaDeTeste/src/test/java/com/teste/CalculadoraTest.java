10e4b85d9aa9cd34b25a001c81c70675fe10ed53
Renan <superesponja15@hotmail.com>
2020-07-17 23:00:22 -0300

    introdução ao js

--- java/AulaDeTeste/src/test/java/com/teste/CalculadoraTest.java
+++ java/AulaDeTeste/src/test/java/com/teste/CalculadoraTest.java
@@ -3,6 +3,8 @@ package com.teste;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculadoraTest {
 
@@ -11,5 +13,15 @@ public class CalculadoraTest {
         Calculadora calc = new Calculadora();
         int soma = calc.somar("1+1+3");
         assertEquals(5, soma);
+    }
+
+    @Test
+    public void testeSomarcomMock() {
+        Calculadora calc = mock(Calculadora.class);
+
+        when(calc.somar("1+2")).thenReturn(10);
+        int resultado = calc.somar("1+2");
+
+        assertEquals(10, resultado);
     }
 }
